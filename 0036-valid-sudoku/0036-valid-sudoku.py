9fcc3ef7112feb40b0dbf60f4ea11601db641587
Sneha B Varghese <b23co026@mace.ac.in>
2026-07-09 01:12:02 +0530

    Time: 8 ms (10.95%), Space: 19.2 MB (94.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,27 +1,4 @@
-# 5 3 . | . 7 . | . . .
-# 6 . . | 1 9 5 | . . .
-# . 9 8 | . . . | . 6 .
-# rows = {}
-# cols = {}
-# boxes = {}
 
-# cols =
-# {
-# 0:{'5','6'},
-# 1:{'3','9'},
-# 2:{'8'}
-# }
-# rows =
-# {
-# 0:{'5','3','7'},
-# 1:{'6','1','9','5'},
-# 2:{'9','8','6'}
-# }
-# boxes =
-# {
-# (0,0):{'5','3','6','9','8'},
-# (0,1):{'7','1','9','5'},
-# }
 from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
