b1b1d6445a403e80655be16e658575c8345e268f
Sneha B Varghese <b23co026@mace.ac.in>
2026-07-09 01:08:06 +0530

    Time: 7 ms (28.93%), Space: 19.3 MB (73.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,8 +1,31 @@
+# 5 3 . | . 7 . | . . .
+# 6 . . | 1 9 5 | . . .
+# . 9 8 | . . . | . 6 .
+# rows = {}
+# cols = {}
+# boxes = {}
+
+# cols =
+# {
+# 0:{'5','6'},
+# 1:{'3','9'},
+# 2:{'8'}
+# }
+# rows =
+# {
+# 0:{'5','3','7'},
+# 1:{'6','1','9','5'},
+# 2:{'9','8','6'}
+# }
+# boxes =
+# {
+# (0,0):{'5','3','6','9','8'},
+# (0,1):{'7','1','9','5'},
+# }
 from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-
-        rows=defaultdict(set)        
+        rows=defaultdict(set)
         cols=defaultdict(set)
         box=defaultdict(set)
         for r in range(9):
@@ -15,5 +38,15 @@ class Solution:
                 rows[r].add(num)
                 cols[c].add(num)
                 box[(r//3,c//3)].add(num)
-                
         return True
+
+
+
+
+
+
+
+
+
+
+        
