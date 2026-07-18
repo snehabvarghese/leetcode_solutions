c946bc469d89fe1011e9c26a3132b91b84468485
Sneha B Varghese <b23co026@mace.ac.in>
2026-07-19 00:26:08 +0530

    Time: 44 ms (89.36%), Space: 19.3 MB (91.11%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,21 +1,14 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        memo={}
-        if len(s1)+len(s2)!=len(s3):
+        n,m=len(s1),len(s2)
+        dp=[[False for _ in range(n+1)]  for _ in range(m+1) ]
+        if n+m!=len(s3):
             return False
-        
-        def dfs(i,j):
-            if i==len(s1) and j==len(s2):
-                return True 
-            if (i,j) in memo:
-                return memo[(i,j)]
-            ans=False
-            if i<len(s1) and s1[i]==s3[i+j]:
-                ans=dfs(i+1,j)
-            if not ans and j<len(s2) and s2[j]==s3[i+j]:
-                ans=dfs(i,j+1)
-            memo[(i,j)]=ans
-            return memo[(i,j)]
-        return dfs(0,0)
-
-        
+        dp[0][0]=True
+        for r in range(m+1):
+            for c in range(n+1):
+                if c>0 and dp[r][c-1] and s1[c-1]==s3[c-1+r]:
+                    dp[r][c]=True
+                if r>0 and dp[r-1][c] and s2[r-1]==s3[r-1+c]:
+                    dp[r][c]=True
+        return dp[-1][-1]
