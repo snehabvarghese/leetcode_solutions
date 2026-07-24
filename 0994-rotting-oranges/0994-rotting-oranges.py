3c5a4bfcc2229afd83a16cc5e27f0d5e50283ffa
Sneha B Varghese <b23co026@mace.ac.in>
2026-07-25 00:00:37 +0530

    Time: 3 ms (87.93%), Space: 12.4 MB (21.94%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,37 +1,42 @@
 from collections import deque
-
 class Solution(object):
     def orangesRotting(self, grid):
-        rows, cols = len(grid), len(grid[0])
+        count=0
+        visited=[[0]*len(grid[0]) for _ in range(len(grid))]
+        q=deque()
+        r=len(grid)
+        c=len(grid[0])
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j]==2:
+                    visited[i][j]=2
+                    q.append((i,j,0))
+        while q:
+            r1,c1,ct=q.popleft()
+            #t,r,b,l
+            
+            count=max(count,ct)
+            dr=[-1,0,1,0]
+            dc=[0,1,0,-1]
+            for i in range(4):
+                new_row=r1+dr[i]
+                new_col=c1+dc[i]
+                if new_row>=0 and new_col>=0 and new_row < r and new_col<c and grid[new_row][new_col]==1 and visited[new_row][new_col]!=2:
+                    q.append((new_row,new_col,ct+1))
+                    visited[new_row][new_col]=2
+         
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j]==1 and visited[i][j]!=2:
+                    return -1
+        return count
+            
+        
 
-        q = deque()
-        fresh = 0
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    q.append((r, c))
-                elif grid[r][c] == 1:
-                    fresh += 1
 
-        minutes = 0
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
 
-        while q and fresh:
-            for _ in range(len(q)):
-                r, c = q.popleft()
+        
 
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-
-                    if (0 <= nr < rows and
-                        0 <= nc < cols and
-                        grid[nr][nc] == 1):
-
-                        grid[nr][nc] = 2
-                        fresh -= 1
-                        q.append((nr, nc))
-
-            minutes += 1
-
-        return minutes if fresh == 0 else -1
+        
+        
