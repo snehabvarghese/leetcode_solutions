81a0927bf6ae31d76742a3f62f05a69044d2d94c
Sneha B Varghese <b23co026@mace.ac.in>
2026-07-28 00:54:02 +0530

    Time: 3 ms (86.96%), Space: 13.1 MB (88.93%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,38 @@
+
+from collections import deque
+
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        adj=[[] for i in range(numCourses)]
+        for u,v in prerequisites:
+            adj[v].append(u)
+        q=deque()
+        indegree=[0]*numCourses
+        for i in range(numCourses):
+            for j in adj[i]:
+                indegree[j]+=1
+        for i in range(numCourses):
+            if indegree[i]==0:
+                q.append(i)
+        topo=[]
+        while q:
+            value=q.popleft()
+            topo.append(value)
+            for nei in adj[value]:
+                
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+            
+        if len(topo)==numCourses:
+            return topo
+        else:
+            return []
+            
+
+
+
+
+
+        
+        
