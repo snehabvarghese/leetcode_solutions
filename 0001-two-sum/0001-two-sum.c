d0dd6f642c040eb5631b460bb2d5872e6c856973
Sneha B Varghese <snehabv14@gmail.com>
2026-09-22 20:36:10 +0530

    Time: 0 ms (100%), Space: 10.5 MB (5.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,20 +1,44 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+#include <stdlib.h>
+typedef struct{
+    int key;
+    int index;
+    int used;
+}Entry;
+
+int hash(int key,int size){
+    return (((key%size)+size)%size);
+};
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int size=numsSize*2+1;
+    Entry* table =calloc(size,sizeof(Entry));
     for (int i=0;i<numsSize;i++){
-        for (int j=i+1;j<numsSize;j++){
-            if (nums[i]+nums[j]==target){
-                int* result=malloc(2*sizeof(int));
-                result[0]=i;
-                result[1]=j;
+        int complement=target-nums[i];
+
+        int pos=hash(complement,size);
+        while(table[pos].used){
+            if (table[pos].key==complement){
+                int* result = malloc(2*sizeof(int));
+                result[1]=i;
+                result[0]=table[pos].index;
                 *returnSize=2;
+                free(table);
+
                 return result;
+
             }
-            
-                    }
+            pos=(pos+1)%size;
+        }
+        pos=hash(nums[i],size);
+        while(table[pos].used){
+            pos=(pos+1)%size;
+
+
+        }
+        table[pos].key=nums[i];
+        table[pos].index=i;
+        table[pos].used=1;
+
     }
     *returnSize=0;
     return NULL;
-    
-}
+};
